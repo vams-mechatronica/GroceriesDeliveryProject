3a0a8961971a386067897c9d89c85ed0a1dd0d9c
anandshekhr <66034323+anandshekhr@users.noreply.github.com>
2023-01-23 22:14:59 +0530

    update for admin quill field

--- vamsgroceriesdelivery/settings.py
+++ vamsgroceriesdelivery/settings.py
@@ -162,12 +162,15 @@ USE_I18N = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
-STATIC_ROOT =  os.path.join(BASE_DIR ,'staticfiles')
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+if DEBUG:
+    STATIC_URL = 'static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+else:
+    STATIC_URL = 'static/'
+    STATIC_ROOT =  os.path.join(BASE_DIR ,'staticfiles')
+    STATICFILES_DIRS = [
+        BASE_DIR / "static"
+    ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
